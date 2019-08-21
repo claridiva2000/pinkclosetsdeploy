97fab3292e9d3f497d52474db9fbf1d0ccb5e6be
claridiva2000 <claridiva2000@gmail.com>
2019-08-21 01:53:24 -0500

    added customer review

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const about = props => {
   return (
-    <div>
-      about
+    <div style={{paddingTop:'150px', fontSize:'1.5rem'}}>
+    <div >
+    <img src="http://pinkclosets.com/owner.png" style={{float:'left', margin:'10px', border:'1px  solid #955465' }}/>
+    <p>There is more to de-cluttering and organizing than meets the eye. The objects in your home can represent a memory or emotion. Over time those objects can pile up, bringing chaos to the sactuary that is your home. We understand that this can be an emotional process and specialize in helping clients who have difficulty "letting go".</p>
+    <p>But trust us, it's worth it!</p>
+    <p>The time saved from knowing EXACTLY where everything is adds hours back into your week which adds up to years added back to your life that you can spend on yourself or your family. And the stress relief of coming home to a beautiful and orderly home will have an amazing effect on your mood and even your health!</p>
+    <h3>"Peace and Order on the Outside Creates Peace and Order on the Inside."</h3>  
+    <p>We do more than just organize closets! We offer an array of services from a small Kitchen Pantry to your large Garage.</p>
+    <p>Dawneshia Webb is the Owner and Founder of Pink|Closets. Growing up she has always had a passion for organizing and arranging. In 2016 Dawneshia decided to step out on faith and turn her passion into a business. Pink|Closets was offially founded in February of 2018.</p>
+    <p>Dawneshia is married with 3 wonderful kids. She has her Associates Degree in Business Administration, and she is also a Certified Lisenced Wedding Coordinator.</p>
+    <p>Pink|Closets is more than just organizing your home, it's organizing your life. The vision of our company is 1 Corithians 14:40.</p>
+    <h3>"But all things should be done decently and in order"</h3>
+    </div>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,9 +20,9 @@ const Home = () => {
           style={{ height: '180vh', marginTop:'-80' }}
         >
           <h2 style={{textShadow:'-2px 2px 3px rgba(150, 150, 150, 1)', position: 'relative',
-  top: '250px'}}>Let all things be done decently and in order.</h2>
+  top: '300px'}}>Let all things be done decently and in order.</h2>
           <h3 style={{textShadow:'-2px 2px 3px rgba(150, 150, 150, 1)', position: 'relative',
-  top: '250px'}}>1 Corinthians 14:40</h3>
+  top: '300px'}}>1 Corinthians 14:40</h3>
 
           <animated.div
             className="box3"
@@ -36,11 +36,29 @@ const Home = () => {
           </animated.div>
         </div>
       </div>
-      <div className="box2" style={{ height: '80vh' }}>
-        <h2 style={{ marginBottom: '500px', width:'80%' }}>
+      <div className="box2" style={{ height: '50vh' }}>
+        <h2 style={{ position:'relative', top:'-100px', width:'80%' }}>
           Pink|Closets is dedicated to helping you get your life in order by
           organizing the things that are a part of you.
         </h2>
+        <p>Join our newsletter!</p>
+        <form style={{ display: 'flex', flexDirection: 'column', justifyContent:'space-between', height:'100px' }}>
+          <input type="text" placeholder="name" style={{padding:'10px', width:'300px'}} />
+          <input type="email" placeholder="email" style={{padding:'10px', width:'300px'}} />
+        </form>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around', 
+            width:'100px',
+            fontSize: '2rem',
+            height:'150px'
+          }}
+        >
+          <i className="fab fa-instagram" />{' '}
+          <i className="fab fa-facebook-square" />
+        </div>
       </div>
       <div className="box1" style={{height:'60vh'}}>
         <h2 style={{marginBottom:'710px'}}>
@@ -49,33 +67,21 @@ const Home = () => {
         </h2>
       </div>
     
+    
      
       <div className="box4" style={{height:'40vh', padding: '30px'}}>
-        <h2>Connect</h2>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-around', 
-            width:'100px',
-            fontSize: '2rem'
-          }}
-        >
-          <i className="fab fa-instagram" />{' '}
-          <i className="fab fa-facebook-square" />
-        </div>
 
-        <p>Join our newsletter!</p>
-        <form style={{ display: 'flex', flexDirection: 'column' }}>
-          <input type="text" placeholder="name" />
-          <input type="email" placeholder="email" />
-        </form>
-        <p>Text Me for a Quote!</p>
+      <div style={{display:'flex', flexDirection:'row',  width:'80%'}}><img src="https://p3plcpnl0791.prod.phx3.secureserver.net:2083/cpsess0578632179/viewer/home%2fj9xj14remmy9%2fpublic_html/Capture.JPG" alt="review1" style={{width:'220px', height:'270px', borderRadius:'100%'}}/> <h4 style={{position: 'relative', bottom:'30px', width:'70%'}}> 
+      <span style={{color:"#955465", fontSize:'4rem'}}>"</span>I hired <span style={{color:"#955465", fontSize:'2rem'}}>Pink|</span>Closets to come help me with a move. My daughters’ room was a mess. They had cleaned up and packed within 2 hours. They were very efficient and that job was done very well. Their prices were very reasonable.  I was impressed with how professional they were. I will be hiring them again very soon to help with two other rooms.<span style={{color:"#955465", fontSize:'4rem'}}>"</span></h4></div>  
+      
+
+       
+        {/* <p>Text Me for a Quote!</p>
         <form style={{ display: 'flex', flexDirection: 'column' }}>
           <input type="text" placeholder="cell#" />
           <input type="text" placeholder="name" />
           <textarea placeholder="message" cols="30" rows="5" />
-        </form>
+        </form> */}
       </div>
     </div>
   );
