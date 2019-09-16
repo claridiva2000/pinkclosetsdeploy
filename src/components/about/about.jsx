0856f1d71cb52a88cceeae80c4ac9a7d49e8fb75
claridiva2000 <claridiva2000@gmail.com>
2019-09-16 01:07:11 -0500

    mvp attained

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -26,10 +26,14 @@ const about = props => {
     <div style={{marginTop:'100px', paddingBottom:'80px',lineHeight:'2rem'}} >
     <img className='Janell' src="https://p3plcpnl0791.prod.phx3.secureserver.net:2083/cpsess4042532781/viewer/home%2fj9xj14remmy9%2fpublic_html/manager.png" alt='owner' />
     
-    <p style={{paddingTop:'50px'}}>Jessica Lander is the team lead of Pink Closets. She’s been with the company since it was founded. 
+    <p style={{paddingTop:'50px'}}>Janell Gines is the office manager for Pink Closets. She’s been with the company since it was founded. 
+
 </p>
-<p>Dawneshia expressed her love of organizing to Jessica, and told her about Pink Closets. Jessica was excited and expressed interest because she shares a love of organizing. Jessica understands how emotionally and physically draining getting your home in order can be. Jessica loves seeing the reaction of the clients once their room is completed. This is one of the big reasons she loves what she does at Pink Closets. </p>
-<p>Jessica has a loving spouse and a beautiful baby girl. She worked retail management for over ten years and has also worked in the customer service field since she was a teenager. </p>
+<p>Janell, like Dawneshia loves to help others to organize their homes. Janell has been in office management for 20 years, so Dawneshia knew she would be a perfect asset to the team. 
+She has four daughters and one son. Being a single mother, great organizing skills were the key to her success. She is very passionate about the vision of Pink Closets, and helps execute it perfectly. 
+ </p>
+<p>Janell is married and has two amazing grandchildren. Because of her recent wedding and new grandchildren, her passion for helping others has only gotten stronger. 
+</p>
     
     </div>
    
